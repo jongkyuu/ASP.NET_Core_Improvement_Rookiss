12934b10a5befc9c8ca9441a7058552b564ab46d
minerva421 <minerva421.ai@gmail.com>
2021-08-11 02:57:05 +0900

    ch1 MVC 복습 폴더 생성

---
+++ ch1_MVC_복습/ch1_MVC_복습/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ch1_MVC_복습.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
