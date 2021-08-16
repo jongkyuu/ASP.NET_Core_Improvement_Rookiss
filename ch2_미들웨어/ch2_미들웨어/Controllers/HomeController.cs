7eb3a61331025c23feac096bfb15cc36878825d9
minerva421 <minerva421.ai@gmail.com>
2021-08-16 19:03:04 +0900

    ch2 미들웨어 실습

--- ch2_미들웨어/ch2_미들웨어/Controllers/HomeController.cs
+++ ch2_미들웨어/ch2_미들웨어/Controllers/HomeController.cs
@@ -26,6 +26,17 @@ namespace ch2_middleWare.Controllers
         // 서버주소/Home/Privacy 주소로 접근
         public IActionResult Privacy()
         {
+            // 에러 나는 상황 만들기(0으로 나눠준 case)
+            int a = 1;
+            int b = 3 / (a - 1);  // 여기서 에러 발생. 
+                                  // EndPoint에서 에러가 위로 전파되면 
+                                  // app.UseExeptionHandler("/Home/Error")가 흐름을 가로채서
+                                  // Request쪽 파이프라인으로 가서 "/Home/Error"에 대한 request가 온 것처럼 요청을 하고 
+                                  // 다시 EndPoint에 가서 라우팅 규칙에 따라 
+                                  // HomeController의 Error() 호출
+
+
+
             // Controller에서 View로 데이터를 넘기는 방법
             ViewData["Message"] = "Data From Privacy";
             // ViewData는 선언해 준 적이 없지만 Controller 안에 구현되어 있음 
--- ch2_미들웨어/ch2_미들웨어/Startup.cs
+++ ch2_미들웨어/ch2_미들웨어/Startup.cs
@@ -41,18 +41,48 @@ namespace ch2_middleWare
         // 4) Controller로 전달 (파이프라인 적용)
         // 5) Controller에서 처리하고 View로 전달 
 
+
+        // HTTP Request Pipeline (NodeJS와 유사)
+        // 미들웨어 : HTTP request / response를 처리하는 중간 부품
+        // [Request]                 [Response]
+        //     [파이프라인]      [파이프라인]
+        //         [마지막 MVC EndPoint]
+
+        // 미들웨어에서 처리한 결과물을 다른 미들웨어로 넘길 수 있다
+        // [!] Controller에서 처리하지 않는 이유는?
+        //   -> 모든 요청마다 로깅을 해야 하는 경우를 예로 들면
+        //      Controller의 모든 코드(메소드)에다가 로깅 기능을 일일이 넣는건 비효율적이고 관리도 힘듬
+        //      이런 식이면 새로운 기능을 추가할때마다 반복적인 작업을 해야함
+        //      일괄적으로 모든 요청에 대해 처리할때 사용하는게 미들웨어
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())  // 개발 상태냐 아니냐에 따라 
-            {
-                app.UseDeveloperExceptionPage();  // DeveloperExceptionPage 로 상세 원인을 보여줌
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");  // 그게 아니면 /Home/Error 에 보냄
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
+            //app.UseStatusCodePages(); // 존재하지 않는 페이지에 접근하면 "Status Code: 404; Not Found" 메세지 출력
+            
+            app.UseExceptionHandler("/Home/Error");  // 맨 밑단에서 Response 쪽으로 타고 올라가다가
+                                                     // Response쪽 파이프라인에서 Error 발생하면
+                                                     // 마치 "/Home/Error"로 Request가 온것처럼
+                                                     // Request쪽 파이프라인에서 다시 타고 내려온다
+                                                     // 응답과 요청 사이가 이런식으로 복잡하게 얽힐 수도 있음
+            
+            // 모든 부품들이 다 필요하지 않다는걸 보여주기 위한 실습
+            //app.UseStaticFiles();
+            //app.UseWelcomePage(); // 중간 부품이라고 해도 순서가 중요할 수 있다
+            //                      // app.UseWelcomePage() 부터 배치하면 아래로 요청을 내려보내지 않고 
+            //                      // 요청을 다 먹어버린 후 (커팅?) 다시 위로 올려보냄(Response쪽으로)
+
+            //if (env.IsDevelopment())  // 개발 상태냐 아니냐에 따라 
+            //{
+            //    app.UseDeveloperExceptionPage();  // DeveloperExceptionPage 로 상세 원인을 보여줌
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");  // 그게 아니면 /Home/Error 에 보냄
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+
             app.UseHttpsRedirection();
 
             // CSS, JavaScript, 이미지 등 요청 받을 때 처리   
